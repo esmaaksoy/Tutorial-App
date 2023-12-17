af64324310f658fa861b49bfa82223bb61c91df7
esma <esma.ea.aksoy@gmail.com>
2023-12-17 21:25:24 +0300

    get axios

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import AddTutorial from "../components/AddTutorial";
 import TutorialList from "../components/TutorialList";
 import axios from "axios";
-const Home =  () => {
-  const getTutorials= async()=>{
-await axios()
-  }
+const Home = () => {
+  const getTutorials = async () => {
+    const url = "https://tutorial-api.fullstack.clarusway.com/tutorials"
+    const res = await axios(url);
+  };
+  getTutorials()
   return (
     <>
       <AddTutorial />
