415ec1b5fd9183f78ee6f8ed501d95e44375f5a1
esma <esma.ea.aksoy@gmail.com>
2023-12-17 21:17:25 +0300

    get

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import AddTutorial from "../components/AddTutorial";
 import TutorialList from "../components/TutorialList";
 
 const Home = () => {
+  const getTutorials=()=>{}
   return (
     <>
       <AddTutorial />
