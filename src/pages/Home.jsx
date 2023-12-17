537e4e95f8513887b2ba7a546302362b7abdc2b8
esma <esma.ea.aksoy@gmail.com>
2023-12-17 21:33:31 +0300

    useEffect

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,18 @@
 import AddTutorial from "../components/AddTutorial";
 import TutorialList from "../components/TutorialList";
 import axios from "axios";
+import { useEffect, useState } from "react";
 const Home = () => {
+  const [tutorials, setTutorials] = useState([])
   const getTutorials = async () => {
     const url = "https://tutorial-api.fullstack.clarusway.com/tutorials"
     const res = await axios(url);
+    setTutorials(res.data);
   };
+  useEffect(()=>{
   getTutorials()
+  }, [])
+
   return (
     <>
       <AddTutorial />
